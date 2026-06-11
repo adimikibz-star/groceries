23393593aa136335f0606b575d78c5d281828813
adimikibz-star <adimikibz@gmail.com>
2026-06-11 11:37:27 +0300

    Initial commit: Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Grocery Shop List - Main Entry Point
+console.log('Grocery Shop List Application Starting...');
+
+// TODO: Implement main application logic
+// Features to implement:
+// - Add items to shopping list
+// - Remove items from list
+// - Mark items as purchased
+// - Organize by category
+// - Track spending and budget
